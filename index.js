97d99df99aeee6764a22e7c6181f0a1630385e59
yoonji lee <lalala703@naver.com>
2022-09-03 22:56:11 +0900

    #9

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+
+const config = require("./config/key");
+
 const { User } = require("./models/User");
 
 app.use(express.json());
@@ -8,13 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://yoon:dlapdlf1@cluster0.2ac5iv3.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("몽고디비 연결됨"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!zzjkjkjkkkkl"));
 
 app.post("/register", (req, res) => {
   // 회원가입할때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
